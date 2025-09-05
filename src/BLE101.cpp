2135d64ca7313f9dec9c653739f6afe898af40b9
Xelz <dratank084mine@gmail.com>
2025-09-05 08:50:47 +0700

    This is initial commit for BLE Fall Advertise project

---
+++ src/BLE101.cpp
@@ -0,0 +1,7 @@
+// #include "BLE101.h"
+
+
+// /*BLE Init*/
+// void BLE_Init(){
+//     Serial.println("BLE Init"); 
+// }
---
+++ src/BLE101.h
@@ -0,0 +1,8 @@
+// #ifndef BlE101_h
+// #define BlE101_h
+
+
+// #include <Arduino.h>
+
+
+// #endif
